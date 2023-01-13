6af069ec28ab4c6e5ecae8df3c6032aaed056cdb
pri65 <priyankasoniii65@gmail.com>
2023-01-13 15:22:55 +0530

    Added media query

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,29 +1,37 @@
 import "./NavbarStyles.css";
-import React from "react";
-import { MenuItems } from './MenuItems';
+import React, { Component } from "react";
+import { MenuItems } from "./MenuItems";
 
-const Navbar = () => {
-  return (
-    <nav className="NavbarItems">
+class Navbar extends Component {
+  state = { clicked: false };
+  handleClick = () => {
+    this.setState({ clicked: !this.state.clicked });
+  };
+  render() {
+    return (
+      <nav className="NavbarItems">
         <h1 className="navbar-logo">Trippy</h1>
-        <div className="menu-icons">
-            <i className="fas fa-bars"></i>
-            <i className="fas fa-times"></i>
+        <div className="menu-icons" onClick={this.handleClick}>
+          <i
+            className={this.state.clicked ? "fas fa-times" : "fas fa-bars"}
+          ></i>
         </div>
-        <ul className="nav-menu">
-            {MenuItems.map((item, index) => {
-                return (
-                    <li key={index}>
-                    <a className={item.cName} href={item.url}>
-                    <i className= {item.icon}></i>{item.title}
-                    </a>
-                </li>
-                );
-            })}
-            <button>Sign Up</button>
+        <ul className={this.state.clicked ? "nav-menu.active" : "nav-menu"}>
+          {MenuItems.map((item, index) => {
+            return (
+              <li key={index}>
+                <a className={item.cName} href={item.url}>
+                  <i className={item.icon}></i>
+                  {item.title}
+                </a>
+              </li>
+            );
+          })}
+          <button>Sign Up</button>
         </ul>
-    </nav>
-  )
+      </nav>
+    );
+  }
 }
 
 export default Navbar;
