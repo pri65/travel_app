3c177f4827c427fd83409bae22cf5327b8c43564
pri65 <priyankasoniii65@gmail.com>
2023-01-13 16:05:19 +0530

    edit routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
+import { Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
-// import { Routes } from 'react-router-dom';
+import { Routes } from 'react-router-dom';
+import Home from './routes/Home';
+import About from './routes/About';
+import Contact from './routes/Contact';
+import Service from './routes/Service';
 import './styles.css';
 
 function App() {
   return (
     <div className="App">
+      <Routes>
+        <Route path='/' element={<Home />}/>
+        <Route path='/about' element={<About />}/>
+        <Route path='/service' element={<Service />}/>
+        <Route path='/contact' element={<Contact />}/>
+      </Routes>
       <Navbar />
     </div>
   );
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,10 +1,11 @@
-function Home () {
-return (
-    <>
-    
-    </>
-)
-}
+import React from 'react';
 
+const Home = () => {
+  return (
+    <div>
+        <h1>Home</h1>
+    </div>
+  )
+}
 
 export default Home;
