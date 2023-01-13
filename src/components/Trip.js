8038051687fd37bfcd2e64b26d9a445e3ef8d9ba
pri65 <priyankasoniii65@gmail.com>
2023-01-13 19:10:31 +0530

    Created trip cards

--- src/components/Trip.js
+++ src/components/Trip.js
@@ -9,6 +9,25 @@ function Trip() {
     <div className="trip">
       <h1>Recent Trips</h1>
       <p>You can discover unique destinations using Google Maps.</p>
+      <div className="tripcard">
+        <TripData
+          image={Trip1}
+          heading="Trip in Indonesia"
+          text="Indonesia, officially the Republic of Indonesia, is a country in Southeast Asia and Oceania between the Indian and Pacific oceans. It consists of over 17,000 islands, including Sumatra, Java, Sulawesi, and parts of Borneo and New Guinea."
+        />
+
+        <TripData
+          image={Trip2}
+          heading="Trip in Malaysia"
+          text="Malaysia is Southeast Asian country occupying parts of the malay Peninsula and the island of Borneo. It's known for its beaches, rainforests and mix of Malay, Chinese, Indian and European cultural influences."
+        />
+
+        <TripData
+          image={Trip3}
+          heading="Trip in France"
+          text="France, in Western Europe, encompasses medieval cities, alpine villages and Mediterranean beaches. Paris, its capital, is famed for its fashion houses, classical art museums including the Louvre and monuments like Eiffel Tower."
+        />
+      </div>
     </div>
   );
 }
