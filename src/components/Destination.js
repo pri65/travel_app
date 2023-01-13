55f2cdb59716234a3ca92085109c89e53b0b03b7
pri65 <priyankasoniii65@gmail.com>
2023-01-13 18:27:53 +0530

    Creating destination data

--- src/components/Destination.js
+++ src/components/Destination.js
@@ -1,7 +1,3 @@
-import Mountain1 from "../assets/1.jpg";
-import Mountain2 from "../assets/2.jpg";
-import Mountain3 from "../assets/5.jpg";
-import Mountain4 from "../assets/8.jpg";
 import DestinationData from "./DestinationData";
 import "./DestinationStyles.css";
 
@@ -10,20 +6,6 @@ const Destination = () => {
     <div className="destination">
       <h1>Popular Destinations</h1>
       <p>Tours give you the opportunity to see a lot, within a time frame.</p>
-      <div className="first-des"></div>
-      <div className="des-text">
-        <h2>Taal Volcano, Batangas</h2>
-        <p>
-          One of the most iconic views in Luzon, Mt. Taal boasts a volcano
-          inside a lake inside an island. If you fancy a closer look, the hike
-          up to the crater is a mere 45 minutes, and is easy enough for
-          beginners. Guides will{" "}
-        </p>
-      </div>
-      <div className="image">
-        <img alt="img" src={Mountain1}/>
-        <img alt="img" src={Mountain2}/>
-      </div>
     </div>
   );
 };
