f88c6013a49f74bd3b963587c5df8403a8a5f64c
pri65 <priyankasoniii65@gmail.com>
2023-01-13 17:07:17 +0530

    Setting hero part

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import Hero from '../components/Hero';
 import Navbar from '../components/Navbar';
 
 const Home = () => {
   return (
     <div>
         <Navbar />
+        <Hero
+        cName="hero"
+        heroImg=""
+        title="your Journey Your Story"
+        text="Choose your Favourite Destination."
+        buttonText="Travel Plan"
+        url="/"
+        btnClass="show"
+        />
     </div>
   )
 }
